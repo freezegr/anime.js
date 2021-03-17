cd4a45b37b3042e425878e74b75a84c162c37faa
freezegr <49855695+freezegr@users.noreply.github.com>
2021-03-17 14:40:08 +0200

    Update util.js

--- src/util.js
+++ src/util.js
@@ -302,139 +302,139 @@ const getMangaList = function (name, status = 'all'){
 module.exports.getMangaList = getMangaList
 
 module.exports.profile = ((name, callback) => {
-	async function getInfo(resolve, reject){
-	let animeStatistic = await getAnimeList(name, 'all')
-	let mangaStatistic = await getMangaList(name, 'all')
-		let profile = {
-			name: name,
-			gender: null,
-			birthday: null,
-			last_online: null,
-			user_pfp: null,
-			friends: [],
-			favorite: {
-				anime: [],
-				manga: [],
-				character: [],
-				people: []
-			},
-			stats: {
-			  anime: {
-				watching: animeStatistic.watching.length,
-	  			completed: animeStatistic.completed.length,
-	  			dropped: animeStatistic.dropped.length,
-	  			planToWatch: animeStatistic.planToWatch.length,
-				episodes: 0,
-			  },
-			  manga: {
-			  	reading: mangaStatistic.reading.length,
-			  	completed: mangaStatistic.completed.length,
-			  	dropped: mangaStatistic.dropped.length,
-			  	planToRead: mangaStatistic.planToRead.length,
-				chapters: 0,
-				volumes: 0
-			  }
-			},
-			anime: {
-				watching: animeStatistic.watching,
-				completed: animeStatistic.completed,
-				dropped: animeStatistic.dropped,
-				planToWatch: animeStatistic.planToWatch 
-			},
-			manga: {
-				reading: mangaStatistic.reading,
-				completed: mangaStatistic.completed,
-				dropped: mangaStatistic.dropped,
-				planToRead: mangaStatistic.planToRead
-			}
-		} 	
-		fetch('https://myanimelist.net/profile/'+name)
-		  .then(ress => ress.text())
-		  .then(res => {
-		  	let $ = cheerio.load(res); 
-			let UserNotFound = $('h1[class="h1"]')
-			if(UserNotFound && $(UserNotFound).html() == "404 Not Found"){
-				return callback(null, `I don\'t know any user with this name "${name}"`)
-			}
-		  	let lolota = $('li[class="clearfix mb12"]')
-		  	  .find('span')
-		  	  .toArray()
-		  	for(let i = 0; i < lolota.length; i++){
-		  		switch ($(lolota[i]).html()){
-		  		  case 'Episodes':
-		  		    profile.stats.anime.episodes = parseFloat($(lolota[i+1]).html().replace(/\,/g, ''))
-		  		  break;
-				  case 'Chapters':
-				    profile.stats.manga.chapters = parseFloat($(lolota[i+1]).html().replace(/\,/g, ''))
-				  break;
-				  case 'Volumes':
-				    profile.stats.manga.volumes = parseFloat($(lolota[i+1]).html().replace(/\,/g, ''))
-				  break;
-		  		}
-		  	}
-		  	let cs = $('li[class="clearfix"]')
-		      .find('span')
-		  	  .toArray()
-			for(let i = 0; i < cs.length; i++){
-			  switch($(cs[i]).html()){
-			    case "Gender":
-				   profile.gender = $(cs[i+1]).html()
-				break;
-				case "Birthday":
-				  profile.birthday = $(cs[i+1]).html()
-				break;
-				case "Last Online":
-				  profile.last_online = $(cs[i+1]).html()
-				break;
-			  }
-			} 
-			let user_pfp = $('div[class="user-image mb8"]').find('img')[0].attribs
-			
-			let favoriteTag = $('ul[class="favorites-list anime"]')
-			  .find('div[class="di-tc va-t pl8 data"] > a')
-			  .toArray()
-			for(let i = 0; i < favoriteTag.length; i++){
-				profile.favorite.anime.push($(favoriteTag[i]).html().replace(/(\s+)/g, ''))
-			}
-			
-			let a_ghost = $('ul[class="favorites-list manga"]')
-			  .find('div[class="di-tc va-t pl8 data"] > a')
-			  .toArray()
-			for(let i = 0; i < a_ghost.length; i++){
-				profile.favorite.manga.push($(a_ghost[i]).html().replace(/(\s+)/g, ''))
-			}
-			let characterTag = $('ul[class="favorites-list characters"]')
-			  .find('div[class="di-tc va-t pl8 data"] > a')
-			  .toArray()
-			for(let i = 0; i < characterTag.length; i++){
-				profile.favorite.character.push($(characterTag[i]).html().replace(/(\s+)/g, ''))
-			}
-			let peopleTag = $('ul[class="favorites-list people"]')
-			  .find('div[class="di-tc va-t pl8 data"] > a')
-			  .toArray()
-			for(let i = 0; i < peopleTag.length; i++){
-				profile.favorite.people.push($(peopleTag[i]).html().replace(/(\s+)/g, ''))
-			}
-			profile.user_pfp = Object.values(user_pfp)[1]
-			fetch(`https://myanimelist.net/profile/${name}/friends`)
-	          .then(ress => ress.text())
-              .then(res => {
-		        let friendsList = []
-                let _ = cheerio.load(res); 
-				let larose = _('div[class="user-friends pt4 pb12"]')
-		          .find('a')
-		  	      .toArray()
-		      for(let i = 0; i < larose.length; i++){
-			    profile.friends.push(_(larose[i]).html())
-		      }  
-			  callback(profile, null);
-		    })
-			
-		    
-		  })
-	}
-	getInfo()
-	
+  async function getInfo(resolve, reject) {
+    let animeStatistic = await getAnimeList(name, 'all')
+    let mangaStatistic = await getMangaList(name, 'all')
+    let profile = {
+      name: name,
+      gender: null,
+      birthday: null,
+      last_online: null,
+      user_pfp: null,
+      friends: [],
+      favorite: {
+        anime: [],
+        manga: [],
+        character: [],
+        people: []
+      },
+      stats: {
+        anime: {
+          watching: animeStatistic.watching.length,
+          completed: animeStatistic.completed.length,
+          dropped: animeStatistic.dropped.length,
+          planToWatch: animeStatistic.planToWatch.length,
+          episodes: 0,
+        },
+        manga: {
+          reading: mangaStatistic.reading.length,
+          completed: mangaStatistic.completed.length,
+          dropped: mangaStatistic.dropped.length,
+          planToRead: mangaStatistic.planToRead.length,
+          chapters: 0,
+          volumes: 0
+        }
+      },
+      anime: {
+        watching: animeStatistic.watching,
+        completed: animeStatistic.completed,
+        dropped: animeStatistic.dropped,
+        planToWatch: animeStatistic.planToWatch
+      },
+      manga: {
+        reading: mangaStatistic.reading,
+        completed: mangaStatistic.completed,
+        dropped: mangaStatistic.dropped,
+        planToRead: mangaStatistic.planToRead
+      }
+    }
+    fetch('https://myanimelist.net/profile/' + name)
+      .then(ress => ress.text())
+      .then(res => {
+        let $ = cheerio.load(res);
+        let UserNotFound = $('h1[class="h1"]')
+        if (UserNotFound && $(UserNotFound).html() == "404 Not Found") {
+          return callback(null, `I don\'t know any user with this name "${name}"`)
+        }
+        let lolota = $('li[class="clearfix mb12"]')
+          .find('span')
+          .toArray()
+        for (let i = 0; i < lolota.length; i++) {
+          switch ($(lolota[i]).html()) {
+            case 'Episodes':
+              profile.stats.anime.episodes = parseFloat($(lolota[i + 1]).html().replace(/\,/g, ''))
+              break;
+            case 'Chapters':
+              profile.stats.manga.chapters = parseFloat($(lolota[i + 1]).html().replace(/\,/g, ''))
+              break;
+            case 'Volumes':
+              profile.stats.manga.volumes = parseFloat($(lolota[i + 1]).html().replace(/\,/g, ''))
+              break;
+          }
+        }
+        let cs = $('li[class="clearfix"]')
+          .find('span')
+          .toArray()
+        for (let i = 0; i < cs.length; i++) {
+          switch ($(cs[i]).html()) {
+            case "Gender":
+              profile.gender = $(cs[i + 1]).html()
+              break;
+            case "Birthday":
+              profile.birthday = $(cs[i + 1]).html()
+              break;
+            case "Last Online":
+              profile.last_online = $(cs[i + 1]).html()
+              break;
+          }
+        }
+        let user_pfp = $('div[class="user-image mb8"]').find('img')[0].attribs
+
+        let favoriteTag = $('ul[class="favorites-list anime"]')
+          .find('div[class="di-tc va-t pl8 data"] > a')
+          .toArray()
+        for (let i = 0; i < favoriteTag.length; i++) {
+          profile.favorite.anime.push($(favoriteTag[i]).html().replace(/(\s+)/g, ''))
+        }
+
+        let a_ghost = $('ul[class="favorites-list manga"]')
+          .find('div[class="di-tc va-t pl8 data"] > a')
+          .toArray()
+        for (let i = 0; i < a_ghost.length; i++) {
+          profile.favorite.manga.push($(a_ghost[i]).html().replace(/(\s+)/g, ''))
+        }
+        let characterTag = $('ul[class="favorites-list characters"]')
+          .find('div[class="di-tc va-t pl8 data"] > a')
+          .toArray()
+        for (let i = 0; i < characterTag.length; i++) {
+          profile.favorite.character.push($(characterTag[i]).html().replace(/(\s+)/g, ''))
+        }
+        let peopleTag = $('ul[class="favorites-list people"]')
+          .find('div[class="di-tc va-t pl8 data"] > a')
+          .toArray()
+        for (let i = 0; i < peopleTag.length; i++) {
+          profile.favorite.people.push($(peopleTag[i]).html().replace(/(\s+)/g, ''))
+        }
+        profile.user_pfp = Object.values(user_pfp)[1]
+        fetch(`https://myanimelist.net/profile/${name}/friends`)
+          .then(ress => ress.text())
+          .then(res => {
+            let friendsList = []
+            let _ = cheerio.load(res);
+            let larose = _('div[class="user-friends pt4 pb12"]')
+              .find('a')
+              .toArray()
+            for (let i = 0; i < larose.length; i++) {
+              profile.friends.push(_(larose[i]).html())
+            }
+            callback(profile, null);
+          })
+
+
+      })
+  }
+  getInfo()
+
 });
 
 module.exports.nsfwAll = function(){
